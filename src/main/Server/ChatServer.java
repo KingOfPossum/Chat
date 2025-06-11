a8494ae91a7cb9d1c683b26c037302755b680846
Paul Mann <paulmann2205@gmail.com>
2025-06-11 12:19:11 +0200

    Handling server listening for client messages in threads

--- src/main/Server/ChatServer.java
+++ src/main/Server/ChatServer.java
@@ -1,34 +1,54 @@
 package main.Server;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class ChatServer {
     private final ServerSocket serverSocket;
-    private BufferedReader reader;
+
+    private final List<Socket> clients = new ArrayList<>();
+    private final HashMap<Socket,BufferedReader> clientReaders = new HashMap<>();
+    private final HashMap<Socket, BufferedWriter> clientWriters = new HashMap<>();
 
     public ChatServer(ServerSocket serverSocket) {
         this.serverSocket = serverSocket;
     }
 
-    public void start() throws IOException {
+    public void start() {
         // Checks for new connections
-        Socket newConnection = this.serverSocket.accept();
-        this.reader = new BufferedReader(new InputStreamReader(newConnection.getInputStream()));
+        try {
+            while(true) {
+                Socket newConnection = this.serverSocket.accept();
 
-        System.out.println("New connection from " + newConnection.getInetAddress().toString());
+                System.out.println("New connection from " + newConnection.getInetAddress().toString());
 
-        listen();
+                this.clients.add(newConnection);
+                this.clientReaders.put(newConnection,new BufferedReader(new InputStreamReader(newConnection.getInputStream())));
+                this.clientWriters.put(newConnection,new BufferedWriter(new OutputStreamWriter(newConnection.getOutputStream())));
+
+                Thread clientListenerThread = new Thread(()->listen(newConnection));
+                clientListenerThread.start();
+            }
+        } catch (IOException e) {
+            System.out.println("Error while accepting new client connection");
+        }
     }
 
-    private void listen() throws IOException {
-        while(true) {
-            String message = reader.readLine();
+    private void listen(Socket clientSocket) {
+        try {
+            BufferedReader clientReader = this.clientReaders.get(clientSocket);
 
-            System.out.println(message);
+            while(true) {
+                String message = clientReader.readLine();
+
+                System.out.println(message);
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading from client!");
         }
     }
 }
